18b57ce6cdef2afe589626bfb60038d059292611
dhruvildds <dhruvildds@gmail.com>
2019-06-10 12:11:49 +0530

    R programming assignment week 3

--- cachematrix.R
+++ cachematrix.R
@@ -15,16 +15,17 @@
 ## setinv() - computes the invererse of the matrix passed
 
 makeCacheMatrix <- function(x = matrix()) {
-i<-NULL
-set<-function(y)
-{x<<-y
-i<<-NULL}
-get<- function() x
-setinv<-function (a) i<-a
-getinv<-function() i
-list(set = set, get = get,
-     setinv = setinv,
-     getinv = getinv)
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
@@ -33,15 +34,17 @@ list(set = set, get = get,
 ## If cached inverse is not available then it returns the computed inverse
 ## It also caches the computed inverse for use later
 
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    i <- x$getinv()
-    if(!is.null(i)) {
-        message("getting cached data")
-        return(i)
-    }
-    data <- x$get()
-    i <- solve(data, ...)
-    x$setinv(i)
-    i
-    }
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
+}
+
